4f1a4093144ee50889a5ff16624f4b23b672f829
Alan Pieczonka <alanpieczonka@gmail.com>
2020-12-17 16:08:46 +0100

    Add /zwroty, /regulamin, under_construction pages and update favicon

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  Spree::Core::Engine.add_routes do
+    get "/polityka-prywatnosci", to: "tos#index", as: :tos
+    get "/zwroty", to: "returns#index", as: :returns
+    get "/", to: "under_construction#index"
+    get "home", to: "home#index"
+  end
 end
